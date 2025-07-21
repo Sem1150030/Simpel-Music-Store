372a99d5f208a5b08682c04b94c42b0d6a00212a
Sem Star <s1150030@student.hsleiden.nl>
2025-07-21 21:59:47 +0200

    started on register

--- Simpel-Music-Store/resources/views/auth/register.blade.php
+++ Simpel-Music-Store/resources/views/auth/register.blade.php
@@ -10,7 +10,7 @@
 
   <div class="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
 
-    <form action="{{ route('login.attempt') }}" method="POST" class="space-y-6">
+    <form action="{{ route('') }}" method="POST" class="space-y-6">
         @csrf
          @if($errors->any())
             <div>

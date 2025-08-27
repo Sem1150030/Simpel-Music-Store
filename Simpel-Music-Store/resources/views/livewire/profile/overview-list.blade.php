d67f43aa1cfb43778c41eb8f23017c96a76b60b6
Sem Star <s1150030@student.hsleiden.nl>
2025-08-27 19:19:38 +0200

    changed margin of overviewlist items in profile

--- Simpel-Music-Store/resources/views/livewire/profile/overview-list.blade.php
+++ Simpel-Music-Store/resources/views/livewire/profile/overview-list.blade.php
@@ -4,7 +4,7 @@
     </div>
 <div class="grid grid-cols-1 md:grid-cols-3 gap-6 p-6">
         <!-- Order History -->
-        <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl p-6 text-center shadow-lg hover:shadow-yellow-500/30 transition">
+        <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl m-2 p-10 text-center shadow-lg hover:shadow-yellow-500/30 transition">
             <h2 class="text-xl font-semibold mb-4 text-[#F4D468]">Order History</h2>
             <p class="text-gray-300 mb-4">View your past purchases and track orders.</p>
             <a href="/profile/orders" class="cursor-pointer px-4 py-2 bg-yellow-500 text-black font-semibold rounded-lg hover:bg-yellow-400">
@@ -13,7 +13,7 @@
         </div>
 
         <!-- User Details -->
-        <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl p-6 text-center shadow-lg hover:shadow-yellow-500/30 transition">
+        <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl m-2 p-10 text-center shadow-lg hover:shadow-yellow-500/30 transition">
             <h2 class="text-xl font-semibold mb-4 text-[#F4D468]">User Details</h2>
             <p class="text-gray-300 mb-4">Update your profile and account settings.</p>
             <a class="cursor-pointer px-4 py-2 bg-yellow-500 text-black font-semibold rounded-lg hover:bg-yellow-400">
@@ -22,7 +22,7 @@
         </div>
 
         <!-- Logout -->
-        <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl p-6 text-center shadow-lg hover:shadow-yellow-500/30 transition">
+        <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl m-2 p-10 text-center shadow-lg hover:shadow-yellow-500/30 transition">
             <h2 class="text-xl font-semibold mb-4 text-[#F4D468]">Logout</h2>
             <p class="text-gray-300 mb-4">Sign out of your account securely.</p>
             <a href="/auth/logout" class="cursor-pointer px-4 py-2 bg-red-600 text-white font-semibold rounded-lg hover:bg-red-500">
@@ -30,7 +30,7 @@
             </a>
         </div>
         @if ($role === 'admin')
-            <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl p-6 text-center shadow-lg hover:shadow-yellow-500/30 transition">
+            <div class="bg-[#111] border border-yellow-500 text-white rounded-2xl m-2 p-10 text-center shadow-lg hover:shadow-yellow-500/30 transition">
                 <h2 class="text-xl font-semibold mb-4 text-[#F4D468]">Backoffice</h2>
                 <p class="text-gray-300 mb-4">Backoffice for administrators</p>
                 <a href="/backoffice/overview" class="cursor-pointer px-4 py-2 bg-[#1c5889] text-white font-semibold rounded-lg hover:bg">
